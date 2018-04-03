b75a20b7e80866ab281ec9259264e43df398647d
David Von Lehman <david.vonlehman@gmail.com>
2018-04-02 22:34:26 -0700

    Lots more work in progress

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-
-
 https://github.com/getpelican/pelican/commit/72756a5c0dba9307331499331357b6848898bc59
-
 
 ## Plugins
 
@@ -9,12 +6,22 @@ Theme uses pelican-frontmark rather as the markdown parser in order to have more
 
 https://github.com/noirbizarre/pelican-frontmark
 
-
 Structured JSON metadata
 https://developers.google.com/search/docs/guides/intro-structured-data
-
 
 https://sourcethemes.com/academic/
 https://sourcethemes.com/academic/docs
 https://themes.gohugo.io/theme/academic/publication/clothing-search/
 https://raw.githubusercontent.com/gcushen/hugo-academic/master/exampleSite/content/publication/clothing-search.md
+
+### Article feature images
+
+Adds visual interest. Use the same image for various metadata including open-graph, JSON/LD, and twitter card. Ideal size is 800x414 (the golden ratio).
+https://blog.bufferapp.com/ideal-image-sizes-social-media-posts
+https://pixabay.com
+
+### Metadata
+
+* JSON/LD metadata
+* [Open Graph](http://ogp.me/)
+* [Twitter Card](https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/markup)
--- static/js/pelican-academic.js
+++ static/js/pelican-academic.js
@@ -1,166 +1,46 @@
-/*************************************************
- *  Academic: the personal website framework for Hugo.
- *  https://github.com/gcushen/hugo-academic
- **************************************************/
-
-// https://github.com/gcushen/hugo-academic/blob/master/static/js/hugo-academic.js
-
-(function($){
-
-  /* ---------------------------------------------------------------------------
-   * Responsive scrolling for URL hashes.
-   * --------------------------------------------------------------------------- */
-
+(function($) {
   // Dynamically get responsive navigation bar offset.
-  let $navbar = $('#navbar_main');
-  let navbar_offset = $navbar.innerHeight();
+  let $navbar = $("#navbar_main");
+  let navbarOffset = $navbar.innerHeight();
 
-  /**
-   * Responsive hash scrolling.
-   * Check for a URL hash as an anchor.
-   * If it exists on current page, scroll to it responsively.
-   * If `target` argument omitted (e.g. after event), assume it's the window's hash.
-   */
-  function scrollToAnchor(target) {
-    // If `target` is undefined or HashChangeEvent object, set it to window's hash.
-    target = (typeof target === 'undefined' || typeof target === 'object') ? window.location.hash : target;
-    // Escape colons from IDs, such as those found in Markdown footnote links.
-    target = target.replace(/:/g, '\\:');
-
-    // If target element exists, scroll to it taking into account fixed navigation bar offset.
-    if($(target).length) {
-      $('body').addClass('scrolling');
-      $('html, body').animate({
-        scrollTop: $(target).offset().top - navbar_offset
-      }, 600, function () {
-        $('body').removeClass('scrolling');
-      });
-    }
-  }
-
-  // Make Scrollspy responsive.
-  function fixScrollspy() {
-    let $body = $('body');
-    let data = $body.data('bs.scrollspy');
-    if (data && data.options) {
-      data.options.offset = navbar_offset;
-      $body.data('bs.scrollspy', data);
-      $body.scrollspy('refresh');
-    }
-  }
-
-  // Check for hash change event and fix responsive offset for hash links (e.g. Markdown footnotes).
-  window.addEventListener("hashchange", scrollToAnchor);
-
-  /* ---------------------------------------------------------------------------
-   * Add smooth scrolling to all links inside the main navbar.
-   * --------------------------------------------------------------------------- */
-
-  $('#navbar_main li.nav-item a').on('click', function(event) {
-    // Store requested URL hash.
-    let hash = this.hash;
-
-    // If we are on the homepage and the navigation bar link is to a homepage section.
-    if ( hash && $(hash).length && ($("#homepage").length > 0)) {
-      // Prevent default click behavior.
-      event.preventDefault();
-
-      // Use jQuery's animate() method for smooth page scrolling.
-      // The numerical parameter specifies the time (ms) taken to scroll to the specified hash.
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top - navbar_offset
-      }, 800);
+  // Smooth scrolling using jQuery easing
+  $('.main-menu__link[href*="#"]:not([href="#"])').click(function() {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - navbarOffset
+          },
+          800
+        );
+        return false;
+      }
     }
   });
 
-  /* ---------------------------------------------------------------------------
-   * Smooth scrolling for Back To Top link.
-   * --------------------------------------------------------------------------- */
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".main-menu__link").click(function() {
+    $(".navbar-collapse").collapse("hide");
+  });
 
-  $('#back_to_top').on('click', function(event) {
+  // Activate scrollspy to add active class to navbar items on scroll
+  if (PAGE_CONTEXT && PAGE_CONTEXT.pageName === "index") {
+    $("body").scrollspy({
+      target: "#navbar_main",
+      offset: navbarOffset
+    });
+  }
+
+  $("#back_to_top").on("click", function(event) {
     event.preventDefault();
-    $('html, body').animate({
-      'scrollTop': 0
-    }, 800, function() {
+    $("html, body").animate({ scrollTop: 0 }, 800, function() {
       window.location.hash = "";
     });
   });
-
-  /* ---------------------------------------------------------------------------
-   * Hide mobile collapsable menu on clicking a link.
-   * --------------------------------------------------------------------------- */
-
-  $(document).on('click', '.navbar-collapse.in', function(e) {
-    //get the <a> element that was clicked, even if the <span> element that is inside the <a> element is e.target
-    let targetElement = $(e.target).is('a') ? $(e.target) : $(e.target).parent();
-
-    if (targetElement.is('a') && targetElement.attr('class') != 'dropdown-toggle') {
-      $(this).collapse('hide');
-    }
-  });
-
-  /* ---------------------------------------------------------------------------
-   * On window load.
-   * --------------------------------------------------------------------------- */
-
-  $(window).on('load', function() {
-    if (window.location.hash) {
-      // When accessing homepage from another page and `#top` hash is set, show top of page (no hash).
-      if (window.location.hash == "#top") {
-        window.location.hash = ""
-      } else {
-        // If URL contains a hash, scroll to target ID taking into account responsive offset.
-        scrollToAnchor();
-      }
-    }
-
-    // Initialize Scrollspy.
-    var $body = $('body');
-    $body.scrollspy({offset: navbar_offset });
-
-    // Call `fixScrollspy` when window is resized.
-    var resizeTimer;
-    $(window).resize(function() {
-      clearTimeout(resizeTimer);
-      resizeTimer = setTimeout(fixScrollspy, 200);
-    });
-
-    // Load citation modal on 'Cite' click.
-    $('.js-cite-modal').click(function(e) {
-      e.preventDefault();
-      let filename = $(this).attr('data-filename');
-      let modal = $('#modal');
-      modal.find('.modal-body').load( filename , function( response, status, xhr ) {
-        if ( status == 'error' ) {
-          let msg = "Error: ";
-          $('#modal-error').html( msg + xhr.status + " " + xhr.statusText );
-        } else {
-          $('.js-download-cite').attr('href', filename);
-        }
-      });
-      modal.modal('show');
-    });
-
-    // Copy citation text on 'Copy' click.
-    $('.js-copy-cite').click(function(e) {
-      e.preventDefault();
-      // Get selection.
-      let range = document.createRange();
-      let code_node = document.querySelector('#modal .modal-body');
-      range.selectNode(code_node);
-      window.getSelection().addRange(range);
-      try {
-        // Execute the copy command.
-        document.execCommand('copy');
-      } catch(e) {
-        console.log('Error: citation copy failed.');
-      }
-      // Remove selection.
-      window.getSelection().removeRange(range);
-    });
-
-    // Initialise Google Maps if necessary.
-    // initMap();
-  });
-
-})(jQuery);
+})(jQuery); // End of use strict
